1d3fc9b2f3e3238d938de83322192a90ff745533
antonioj562 <antoniojloyola@gmail.com>
2026-01-05 16:03:29 -0800

    Added a former board section - added github and linkedin icons to members if available

---
+++ src/data/formerBoard.ts
@@ -0,0 +1,65 @@
+import kaylaImg from "../assets/kayla.png";
+import chrisImg from "../assets/Chris.jpg";
+import justinImg from "../assets/justin.png";
+import amarImg from "../assets/amar.png";
+import phuImg from "../assets/phu.png";
+import danielImg from "../assets/daniel.jpg";
+import nicholasImg from "../assets/nicholas.jpg";
+import placeholder from "../assets/placeholder.png";
+
+
+export const formerBoard = [
+	{
+		name: "Justin",
+		title: "President",
+		img: justinImg,
+		github: "https://github.com/chillwafflez",
+		linkedin: "https://www.linkedin.com/in/justin-mn/",
+		desc: "Hello! My name is Justin, and I previously served as Co-President of Computer Science Society. I’m a 4th year CS major passionate about building community, supporting members, and planning engaging activities for CSS."
+	},
+	{
+		name: "Kayla",
+		title: "President",
+		img: kaylaImg,
+		desc: "Hello! I’m Kayla, a 4th year computer science major at Cal Poly Pomona and served as CSS President for 2023–2024. I worked on projects such as the Northrop Grumman Collaboration Project, PolySec Cyber Lab, and HAPII Lab, and interned at NASA’s Jet Propulsion Lab. My advice is to get involved and take advantage of the opportunities and resources available at CPP."
+	},
+	{
+        name: "Daniel",
+        title: "Vice President",
+        img: danielImg,
+		github: "https://github.com/DanielPasion",
+		linkedin: "https://www.linkedin.com/in/danielpasion/",
+        desc: "Hello! My name is Daniel, and I’m honored to serve as the Vice President of Computer Science this year. I'm currently a 4th year studying Computer Science and am passionate about making a positive impact within our club and campus community. In my role as Vice President, I’m excited to help plan engaging activities, support our members, and work alongside an incredible team. I can’t wait to connect with all of you and make this year a memorable one for everyone involved!"
+    },
+	{
+		name: "Amar",
+		title: "Vice President",
+		img: amarImg,
+		desc: "Hi everyone! I’m Amar, a 4th year CS student and former CSS Vice President. My technical background is in cloud computing and infrastructure, with internship experience at VMware and The Aerospace Corporation."
+	},
+	{
+		name: "Phu",
+		title: "Secretary",
+		img: phuImg,
+		desc: "Yooo, I’m Phu Truong, a 4th year CS student at CPP. Growing up in Vietnam sparked my interest in technology after moving to the U.S. I love learning about computers and software and enjoyed serving CSS as Secretary."
+	},
+	{
+		name: "Jeremy",
+		title: "Treasurer",
+		img: placeholder,
+		desc: "Hello!! I’m Jeremy, a 4th year CS major who worked on web development projects and explored software and mobile app development. Outside of CS, I enjoy spending time with friends and student orgs like NSU."
+	},
+	{
+		name: "Chris",
+		title: "Treasurer",
+		img: chrisImg,
+		desc: "Hello! I’m Christopher Lo, a 3rd year student at CPP. I’m currently most comfortable with backend related topics, but I’m hoping to learn AI-related things. Looking forward to learning and growing with everyone this semester!"
+	},
+    {
+        name: "Nicholas",
+        title: "Webmaster",
+        img: nicholasImg,
+		github: "https://github.com/Nickthecan",
+        desc: "Hello everyone. My name is Nicholas, and I am serving as the Webmaster for Computer Science Society. I am currently a 4th year studying Computer Science and I'm proud to be a part of this club (community even). I hope to improve the overall web design for our club this year. In my free time, you can often see me rock climbing or hanging out with friends in a circle underneath a tree in the campus plaza. I hope to see you around the club room or around campus! It's great to meet new faces!"
+    }
+];

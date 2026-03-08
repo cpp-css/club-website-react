acd1de3ff67b40916f4f6f23577836a30a8d9c6f
umarazizadah <137125325+umarazizadah@users.noreply.github.com>
2026-03-08 16:23:26 -0700

    chore(main): Integrate redesign styling pipeline and Tailwind setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,34 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/main.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-import Rootlayout from "./layouts/Rootlayout.tsx"
-import { Contact } from "./pages/Contact.tsx"
-import { EBoard } from "./pages/E_Board.tsx"
-import { Events } from "./pages/Events.tsx"
-import { Projects } from "./pages/Projects.tsx"
-import { Home } from "./pages/Home.tsx"
+import Rootlayout from "./layouts/Rootlayout.tsx";
+import { Contact } from "./pages/Contact.tsx";
+import { EBoard } from "./pages/E_Board.tsx";
+import { Events } from "./pages/Events.tsx";
+import { Projects } from "./pages/Projects.tsx";
+import { Home } from "./pages/Home.tsx";
 
-const router = createBrowserRouter([
-  {
-    element: <Rootlayout />,
-    children: [
-      { path: "/", element: <Home /> },
-      { path: "/e-board", element: <EBoard /> },
-      { path: "/events", element: <Events /> },
-      { path: "/projects", element: <Projects /> },
-      { path: "/contact", element: <Contact /> },
-    ]
-  }],
+// redesign global style
+import "./styles/index.css";
+
+const router = createBrowserRouter(
+  [
+    {
+      element: <Rootlayout />,
+      children: [
+        { path: "/", element: <Home /> },
+        { path: "/e-board", element: <EBoard /> },
+        { path: "/events", element: <Events /> },
+        { path: "/projects", element: <Projects /> },
+        { path: "/contact", element: <Contact /> },
+      ],
+    },
+  ],
   {
     basename: "/",
-  }
+  },
 );
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <RouterProvider router={router} />
   </StrictMode>,
-)
+);
